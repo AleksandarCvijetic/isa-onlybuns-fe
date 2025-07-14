8e9aa3c636e6499e009918e2e3299c2e35d960e7
MilanUD <149141569+MilanUD@users.noreply.github.com>
2025-07-14 21:26:26 +0200

    feat: added commenting on the post

--- src/main.js
+++ src/main.js
@@ -6,12 +6,26 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faComments, faUser } from '@fortawesome/free-solid-svg-icons';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
 library.add(faComments, faUser);
 
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 const app = createApp(App)
 
+
+
 app.use(router)
+
+app.use(vuetify);
 
 app.component('font-awesome-icon', FontAwesomeIcon);
 
